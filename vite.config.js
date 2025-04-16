5b9cab37582026169ced5ba3ac4b72e5e34ab996
blackcatter123 <zhaobingzhe1232021@163.com>
2025-04-16 22:04:21 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vite.dev/config/
+export default defineConfig({
+  // base: "/security.github.io/",
+  plugins: [vue()],
+});
