d87e5511fcb96276dcc7ed803fb0bb9df45330eb
blackcatter123 <zhaobingzhe1232021@163.com>
2025-04-18 18:26:27 +0800

    update_of_hiddenMsg

---
+++ src/js/hiddenPage.js
@@ -0,0 +1,6 @@
+let slider = document.querySelector(".slider");
+let defaultNum = 0;
+slider.addEventListener("click", () => {});
+function switchColor() {
+  let chosenModeArr = document.querySelectorAll(".chosenMode");
+}
