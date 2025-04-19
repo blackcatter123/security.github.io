e77a5348de3ecc42b02fe1285fea8fd8814587fb
blackcatter123 <zhaobingzhe1232021@163.com>
2025-04-19 11:00:00 +0800

    security 2.0

--- src/js/hiddenPage.js
+++ src/js/hiddenPage.js
@@ -1,6 +1,16 @@
 let slider = document.querySelector(".slider");
+let chosenModeArr = document.querySelectorAll(".chosenMode");
 let defaultNum = 0;
-slider.addEventListener("click", () => {});
-function switchColor() {
-  let chosenModeArr = document.querySelectorAll(".chosenMode");
+chosenModeArr[defaultNum].classList.add("beChosenStyle");
+slider.addEventListener("click", () => {
+  if (defaultNum == 0) {
+    defaultNum = 1;
+  } else {
+    defaultNum = 0;
+  }
+  switchColor(defaultNum);
+});
+function switchColor(index) {
+  document.querySelector(".beChosenStyle").classList.remove("beChosenStyle");
+  chosenModeArr[index].classList.add("beChosenStyle");
 }
